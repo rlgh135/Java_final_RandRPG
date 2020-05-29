c13cfacfa470c58f37786a5add4b9a5eb85f8477
rlgh135 <66102848+rlgh135@users.noreply.github.com>
2020-05-29 17:26:31 +0900

    Update skillStructure.java

--- Java_Final_RandRPG/src/Java_Final_RandRPG/skillStructure.java
+++ Java_Final_RandRPG/src/Java_Final_RandRPG/skillStructure.java
@@ -1,5 +1,18 @@
-package Java_Final_RandRPG;
-
 public class skillStructure {
+	int Skill_No;
+	int ChoiceTimes;
+	int UseMp;
+	String SkillName;
+	String ToolTip;
+	
+	
+	public skillStructure(){
+		Skill_No=-1;
+		SkillName="";
+		ToolTip="";
+		ChoiceTimes=0;
+		UseMp=0;
+		
+	}
 
 }
